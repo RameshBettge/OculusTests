561a19560cc3e32abe2fda96fe1aec691af4ff03
RameshBettge <bettge.r@gmx.de>
2018-11-21 13:00:19 +0100

    Initial Commit: Standard VR Project with samples. ADDED: Script which prints all key/button input to the console. VRINputLookup (scriptable Object)

---
+++ Assets/_Scripts/CheckInput.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class CheckInput : MonoBehaviour
+{
+    private void Awake()
+    {
+        //ReadAxes();
+    }
+
+    void Update()
+    {
+        ReadKeys();
+        //ReadJoysticks();
+        //ReadButtons();
+        //ReadAxes();
+    }
+
+    private void ReadButtons()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            if (Input.GetKeyDown("joystick 1 button " + i))
+            {
+                Debug.Log("joystick " + i + " button pressed");
+            }
+        }
+    }
+
+    private void ReadJoysticks()
+    {
+        var joysticks = Input.GetJoystickNames();
+
+        foreach(var s in joysticks)
+        {
+            print(s);
+            //float input = Input.GetAxisRaw(s);
+            //if (input != 0f)
+            //{
+            //    print(s + ": " + input);
+            //}
+        }
+
+    }
+
+    void ReadAxes()
+    {
+        var inputManager = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/InputManager.asset")[0];
+
+        SerializedObject obj = new SerializedObject(inputManager);
+
+        SerializedProperty axisArray = obj.FindProperty("m_Axes");
+
+        if (axisArray.arraySize == 0)
+            Debug.Log("No Axes");
+
+        for (int i = 0; i < axisArray.arraySize; ++i)
+        {
+            var axis = axisArray.GetArrayElementAtIndex(i);
+
+            var name = axis.FindPropertyRelative("m_Name").stringValue;
+            var axisVal = axis.FindPropertyRelative("axis").intValue;
+            //int inputType = (InputType)axis.FindPropertyRelative("type").intValue;
+
+            if (axisVal < 0.01f || axisVal > -0.5f) { return;} //no input
+
+            Debug.Log(name);
+            Debug.Log(axisVal);
+            //Debug.Log(inputType);
+        }
+    }
+
+    void ReadKeys()
+    {
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                Debug.Log(key.ToString());
+            }
+        }
+
+        
+    }
+}
---
+++ Assets/_Scripts/InputLookups/VRInputLookup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+// For every supported VR Headset, create one  class, which inherits from this.
+// Assign every button.
+[CreateAssetMenu(fileName = "VRInputLookup", menuName = "Lookups/VRInput", order = 1)]
+public class VRInputLookup : ScriptableObject
+{
+    [Header("Right Controller")]
+    public KeyCode rButton1 = KeyCode.Joystick1Button0;
+    public KeyCode rButton1_Touch = KeyCode.Joystick1Button0;
+    [Space(10)]
+
+    public KeyCode rButton2 = KeyCode.Joystick1Button0;
+    public KeyCode rButton2_Touch = KeyCode.Joystick1Button0;
+    [Space(10)]
+
+
+    public string rJoyH = "Horizontal";
+    public string rJoyV = "Horizontal";
+    [Space(10)]
+
+    [Header("Left Controller")]
+    public KeyCode lButton1 = KeyCode.Joystick1Button0;
+    public KeyCode lButton1_Touch = KeyCode.Joystick1Button0;
+    [Space(10)]
+
+    public KeyCode lButton2 = KeyCode.Joystick1Button0;
+    public KeyCode lButton2_Touch = KeyCode.Joystick1Button0;
+    [Space(10)]
+
+    public string lJoyH = "Horizontal";
+    public string lJoyV = "Horizontal";
+}
