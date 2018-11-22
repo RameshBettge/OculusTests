d261878d68a48f15d6ddb91d0d2d87762608720e
RameshBettge <bettge.r@gmx.de>
2018-11-22 09:40:44 +0100

    ADDED: VRController.cs which will make it easier to organize VRInputLookups.

---
+++ Assets/_Scripts/InputLookups/VRController.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+
+[System.Serializable]
+public class VRController
+{
+    [SerializeField] int button1 = 0;
+    [SerializeField] int button1_Touch = 0;
+    [Space(10)]
+
+    [SerializeField] int button2 = 0;
+    [SerializeField] int button2_Touch = 0;
+    [Space(10)]
+
+    [SerializeField] string thumbX = "Horizontal";
+    [SerializeField] string thumbY = "Horizontal";
+    [SerializeField] int thumb_Touch = 0;
+    [SerializeField] int thumb_Press = 0;
+    [Space(10)]
+
+
+    [SerializeField] string index = "Horizontal";
+    //string rIndex_NearTouch = "Horizontal";
+    [SerializeField] int index_Touch = 0;
+    [Space(5)]
+    [SerializeField] string grab = "Horizontal";
+
+    // ----
+
+    public int Button1 = 0;
+    public int Button1_Touch = 0;
+    [Space(10)]
+
+    public int Button2 = 0;
+    public int Button2_Touch = 0;
+    [Space(10)]
+
+    public string ThumbX = "Horizontal";
+    public string ThumbY = "Horizontal";
+    public int Thumb_Touch = 0;
+    public int Thumb_Press = 0;
+    [Space(10)]
+
+
+    public string Index = "Horizontal";
+    //string rIndex_NearTouch = "Horizontal";
+    public int Index_Touch = 0;
+    [Space(5)]
+    public string Grab = "Horizontal";
+}
--- Assets/_Scripts/InputLookups/VRInputLookup.cs
+++ Assets/_Scripts/InputLookups/VRInputLookup.cs
@@ -5,6 +5,9 @@
 [CreateAssetMenu(fileName = "VRInputLookup", menuName = "Lookups/VRInput", order = 1)]
 public class VRInputLookup : ScriptableObject
 {
+    public VRController Right;
+    public VRController Left;
+
     [Header("Right Controller")]
     public KeyCode rButton1 = KeyCode.Joystick1Button0;
     public KeyCode rButton1_Touch = KeyCode.Joystick1Button0;
